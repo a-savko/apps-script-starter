47b9eee9ec39b15b2f3aff40c6f8d0ce87ea94e1
Aliaksei Sauko <a.savko@itransition.com>
2024-06-14 19:22:41 +0300

    removed examples

--- test/server/utils.test.js
+++ test/server/utils.test.js
@@ -1,10 +1,14 @@
 import assert from 'assert';
-import { hasCpuTime } from '../../src/server/utils';
 
-describe('can get CPU time', () => {
-  it('CPU time should be larger than 0 at all times', () => {
-    const result = hasCpuTime();
-    assert.notStrictEqual(result, 0);
-    expect(result).not.toBe(0);
+describe('feature description', () => {
+  it('test description', () => {
+    // 
+    // Arrange
+
+    // 
+    // Act
+
+    //
+    // Assert
   });
 });
