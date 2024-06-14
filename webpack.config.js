e9d0601e718942280afff263524b3d3dea5e98bd
Aliaksei Sauko <a.savko@itransition.com>
2024-06-14 18:47:17 +0300

    webpack, added .env processing to the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,94 +13,109 @@ const GasPlugin = require('gas-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const getSrcPath = (filePath) => {
   const src = path.resolve(__dirname, 'src');
   return path.posix.join(src.replace(/\\/g, '/'), filePath);
 };
 
-module.exports = {
-  mode: 'production',
-  context: __dirname,
-  entry: getSrcPath('/index.js'),
-  output: {
-    filename: `code.js`,
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        test: /\.js$/i,
-        extractComments: false,
-        terserOptions: {
-          ecma: 2020,
-          compress: true,
-          mangle: {
-            reserved: ['global'],
-            keep_fnames: true, // Easier debugging in the browser
+module.exports = (env, argv) => {
+  let mode = 'none';
+  if(argv.mode === 'production' || argv.mode === 'development'){
+    mode = argv.mode;
+  }
+
+  const envPath = argv.mode === 'production' ? './.env' : `./.env.${env.ENV_GOAL}`;
+
+  const config = {
+    mode,
+    context: __dirname,
+    entry: getSrcPath('/index.js'),
+    output: {
+      filename: `code.js`,
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          test: /\.js$/i,
+          extractComments: false,
+          terserOptions: {
+            ecma: 2020,
+            compress: true,
+            mangle: {
+              reserved: ['global'],
+              keep_fnames: true, // Easier debugging in the browser
+            },
+            format: {
+              comments: /@customfunction/i,
+            },
           },
-          format: {
-            comments: /@customfunction/i,
+        }),
+      ],
+    },
+    performance: {
+      hints: false,
+    },
+    watchOptions: {
+      ignored: ['**/dist', '**/node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+            },
           },
-        },
-      }),
-    ],
-  },
-  performance: {
-    hints: false,
-  },
-  watchOptions: {
-    ignored: ['**/dist', '**/node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            cacheCompression: false,
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new ESLintPlugin(),
-    new webpack.ProgressPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: getSrcPath('**/*.html'),
-          to: '[name][ext]',
-          noErrorOnMissing: true,
-        },
-        {
-          from: getSrcPath('../appsscript.json'),
-          to: '[name][ext]',
-        },
-        {
-          from: getSrcPath('../functions/*.js'),
-          to: '[name][ext]',
-          noErrorOnMissing: true,
-          info: { minimized: true },
-        },
-        {
-          from: getSrcPath('../node_modules/apps-script-oauth2/dist/OAuth2.gs'),
-          to: 'OAuth2.js',
         },
       ],
-    }),
-    new GasPlugin({
-      comments: false,
-      source: 'digitalinspiration.com',
-    }),
-  ],
+    },
+    plugins: [
+      new ESLintPlugin(),
+      new webpack.ProgressPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: getSrcPath('**/*.html'),
+            to: '[name][ext]',
+            noErrorOnMissing: true,
+          },
+          {
+            from: getSrcPath('../appsscript.json'),
+            to: '[name][ext]',
+          },
+          {
+            from: getSrcPath('../functions/*.js'),
+            to: '[name][ext]',
+            noErrorOnMissing: true,
+            info: { minimized: true },
+          },
+          {
+            from: getSrcPath('../node_modules/apps-script-oauth2/dist/OAuth2.gs'),
+            to: 'OAuth2.js',
+          },
+        ],
+      }),
+      new GasPlugin({
+        comments: false,
+        source: 'digitalinspiration.com',
+      }),
+      new Dotenv({
+        path: envPath,
+      }),
+    ],
+  };
+   
+  return config;
 };
